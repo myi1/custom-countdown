9b322ba5f7209978fd16d4250390f77e55675c1d
Muhammad Ismail <yahyaismail@gmail.com>
2021-08-23 21:39:55 -0400

    Add reset functionality

--- script.js
+++ script.js
@@ -7,10 +7,15 @@ const countdownElTitle = document.getElementById("countdown-title");
 const countdownBtn = document.getElementById("countdown-button");
 const timeElements = document.querySelectorAll("span");
 
+const completeEl = document.getElementById("complete");
+const completeElInfo = document.getElementById("complete-info");
+const completeBtn = document.getElementById("complete-button");
+
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
 let countdownActive;
+let savedCountdown;
 
 const second = 1000;
 const minute = second * 60;
@@ -26,25 +31,34 @@ function updateDOM() {
   countdownActive = setInterval(() => {
     const now = new Date().getTime();
     const distance = countdownValue - now;
-    console.log("distance", distance);
 
     const days = Math.floor(distance / day);
     const hours = Math.floor((distance % day) / hour);
     const minutes = Math.floor((distance % hour) / minute);
     const seconds = Math.floor((distance % minute) / second);
-    console.log(days, hours, minutes, seconds);
-
-    //   Populate countdown values
-    countdownElTitle.textContent = `${countdownTitle}`;
-    timeElements[0].textContent = `${days}`;
-    timeElements[1].textContent = `${hours}`;
-    timeElements[2].textContent = `${minutes}`;
-    timeElements[3].textContent = `${seconds}`;
 
     //   Hide Input Container
     inputContainer.hidden = true;
-    // Show Countdown
-    countdownEl.hidden = false;
+
+    // If countdown has ended, show complete
+    if (distance < 0) {
+      countdownEl.hidden = true;
+      clearInterval(countdownActive);
+      completeElInfo.textContent = `${countdownTitle} finished on ${countdownDate}`;
+      completeEl.hidden = false;
+    } else {
+      // Else, show countdown in progress
+      //   Populate countdown values
+      countdownElTitle.textContent = `${countdownTitle}`;
+      timeElements[0].textContent = `${days}`;
+      timeElements[1].textContent = `${hours}`;
+      timeElements[2].textContent = `${minutes}`;
+      timeElements[3].textContent = `${seconds}`;
+
+      // Show Countdown
+      countdownEl.hidden = false;
+      countdownEl.hidden = false;
+    }
   }, second);
 }
 
@@ -53,14 +67,19 @@ function updateCountdown(e) {
   e.preventDefault();
   countdownTitle = e.target.title.value;
   countdownDate = e.srcElement[1].value;
-  console.log(countdownDate, countdownTitle);
+  savedCountdown = {
+    title: countdownTitle,
+    date: countdownDate,
+  };
+  localStorage.setItem("countdown", JSON.stringify(savedCountdown));
+
   //   Check for Valid Date
   if (countdownDate === "") {
     alert("Please select a date for the countdown.");
   } else {
     //   Get number vewrsion of current Date, update DOM
     countdownValue = new Date(countdownDate).getTime();
-    console.log("countdown value:", countdownValue);
+
     updateDOM();
   }
 }
@@ -69,14 +88,32 @@ function updateCountdown(e) {
 function reset() {
   // Hide countdowns, show Input
   countdownEl.hidden = true;
+  completeEl.hidden = true;
   inputContainer.hidden = false;
   // Stop the Countdown
   clearInterval(countdownActive);
   // Reset values
   countdownTitle = "";
   countdownDate = "";
+  localStorage.removeItem("countdown");
 }
 
-// Event Listener
+function restorePreviousCountdown() {
+  // Get countdown from localStorage if available
+  if (localStorage.getItem("countdown")) {
+    inputContainer.hidden = true;
+    savedCountdown = JSON.parse(localStorage.getItem("countdown"));
+    countdownTitle = savedCountdown.title;
+    countdownDate = savedCountdown.date;
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
+}
+
+// Event Listeners
 countdownForm.addEventListener("submit", updateCountdown);
 countdownBtn.addEventListener("click", reset);
+completeBtn.addEventListener("click", reset);
+
+// On Load, check localStorage
+restorePreviousCountdown();
